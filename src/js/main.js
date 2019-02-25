08218a42e04bb505d6daa04b8603d0d23e200c51
Juweek Adolphe <jadolphe23@gmail.com>
2019-02-25 10:37:09 -0500

    native women code

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 // Javascript that is inline. Should be used for anything that needs to be immediate
-window.$ = require('./vendor/jquery.js');
-
-var share = require('./modules/share.js');
-
-share.init();
+document.getElementsByClassName('video__cta')[0].addEventListener('click', function() {
+    setTimeout(function() {
+        document.getElementsByTagName('body')[0].classList.add('has-clicked');
+    }, 200);
+    document.getElementsByClassName('video__embed')[0].setAttribute('src', 'https://www.youtube.com/embed/PubiIdVO7-w?autoplay=1');
+})
